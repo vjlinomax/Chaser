492d1265b43a7dd78ab4ff679429fede7943c674
Joris de Jong <joris@hybridvisuals.nl>
2016-12-17 21:07:07 +0100

    UpdateableComponent

---
+++ Source/gui logic/UpdateableComponent.cpp
@@ -0,0 +1,21 @@
+/*
+  ==============================================================================
+
+    UpdateableComponent.cpp
+    Created: 17 Dec 2016 8:34:36pm
+    Author:  Joris
+
+  ==============================================================================
+*/
+
+#include "UpdateableComponent.h"
+
+UpdateableComponent::UpdateableComponent()
+{
+
+}
+
+UpdateableComponent::~UpdateableComponent()
+{
+
+}
---
+++ Source/gui logic/UpdateableComponent.h
@@ -0,0 +1,29 @@
+/*
+  ==============================================================================
+
+    UpdateableComponent.h
+    Created: 17 Dec 2016 8:34:36pm
+    Author:  Joris
+
+  ==============================================================================
+*/
+
+#ifndef UPDATEABLECOMPONENT_H_INCLUDED
+#define UPDATEABLECOMPONENT_H_INCLUDED
+
+//we use this so we can store a bunch of components in a manager
+//and let them decide themselves what needs to happen 
+//when they need to be updated
+
+class UpdateableComponent
+{
+public:
+	UpdateableComponent();
+	~UpdateableComponent();
+
+	virtual void update() = 0;
+};
+
+
+
+#endif  // UPDATEABLECOMPONENT_H_INCLUDED
